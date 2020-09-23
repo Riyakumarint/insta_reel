6466fcdf7beb073ef7b2b70916d3fd1b411319ba
Riyakumarint <riyakumaribkj@gmail.com>
2020-09-23 10:48:43 +0530

    app

--- insta_reel_clone/src/App.js
+++ insta_reel_clone/src/App.js
@@ -11,3 +11,14 @@ function App() {
 }
 
 export default App;
+
+
+/* IG Reels Clone built with React
+Deploy using Firebase 
+Videos are stored in Firebase 
+Using Firebase
+CSS Tricks + Hacks
+How the Tik tok style Vertical Snapping Word
+Material UI
+React Ticker... (For scrolling ticker) 
+You'll learn REACT HOOKS */
